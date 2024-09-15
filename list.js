9c3dd2e19a2dcfb5aaf009d469b9b50ec8790fb7
siroshimadenagasaki <161388775+siroshimadenagasaki@users.noreply.github.com>
2024-09-15 20:08:27 -0300

    beeeee

--- list.js
+++ list.js
@@ -21,14 +21,340 @@ const solomonlist = [
     },
     {
         "solomonCode": "3-3B",
-        "link": "www.google.com",
+        "link": "www.blank.com",
         "name": " Major/minor triad n2 "
     },
       {
+        "solomonCode": "3-4",
+        "link": "https://drive.google.com/file/d/11G26XcGI1vDzvukLQiA6C4wiz5LbZnQH/view?usp=drive_link",
+        "name": "Maj7(no5) - incomplete Maj7 n1"
+    },
+    {
+        "solomonCode": "3-4B",
+        "link": "www.blank.com",
+        "name": "Maj7(no3) - incomplete Maj7 n2"
+    },      {
+        "solomonCode": "3-5",
+        "link": "https://drive.google.com/file/d/1y5sfPFCGkDWUiXiV9jFyLZbNBdFaFU2z/view?usp=drive_link",
+        "name": "Fourth/Tritone triad - Rite n1 - Lydian triad"
+    },      {
+        "solomonCode": "3-5B",
+        "link": "https://drive.google.com/file/d/1jFrR8JjTuGHKMKzampx9P2ZHxWSbo8YQ/view?usp=drive_link",
+        "name": "Tritone/Fourth triad - Rite n2 - Phrygian triad n2"
+    },      {
+        "solomonCode": "3-6",
+        "link": "https://drive.google.com/file/d/1jhF-ioYnp7Ux7xYxhTxH0hACVEqmwume/view?usp=drive_link",
+        "name": "Tonal triad"
+    },      {
+        "solomonCode": "3-7",
+        "link": "https://drive.google.com/file/d/1Gj7n9HHUye9521BDmABnqwhdfbXwiE4V/view?usp=drive_link",
+        "name": "m7(no5) - Incomplete minor seventh"
+    },      {
+        "solomonCode": "3-7B",
+        "link": "https://drive.google.com/file/d/1Ss6UMiJsLbJUxkkkma8umnPKuBs0svcT/view?usp=drive_link",
+        "name": "7(no3) - Incomplete dominant triad n1"
+    },      {
+        "solomonCode": "3-8",
+        "link": "https://drive.google.com/file/d/1Ld9ltyWInviSO52W_xjXuD51vGddJMUN/view?usp=drive_link",
+        "name": "7(no5) - Incomplete dominant triad n2"    },     
+     {
+        "solomonCode": "3-8B",
+        "link": "https://drive.google.com/file/d/1bHV3UnYhdheC31krMxcdjwD8imdNGQAY/view?usp=drive_link",
+        "name": "5b triad - 1 3 5b "
+    },      {
+        "solomonCode": "3-9",
+        "link": "https://drive.google.com/file/d/1_yVMuQMVDMGIFRVZ7cqPxupL07FRBdrq/view?usp=drive_link",
+        "name": "Cuartal triad"
+    },      {
+        "solomonCode": "3-10",
+        "link": "https://drive.google.com/file/d/1YSoV--g83b9eGJn2TuHXvfVFSyeYm4OA/view?usp=drive_link",
+        "name": "Diminished triad - m6(no5)"
+    },      {
+        "solomonCode": "3-11",
+        "link": "https://drive.google.com/file/d/1rxkvSrXy8PUCd7TXnGJaB6V4xx_5MQx4/view?usp=drive_link",
+        "name": " Minor triad - 6(no5)"
+    },      {
+        "solomonCode": "3-11B",
+        "link": "",
+        "name": "Major triad - m6(no5)"
+    },         {
+        "solomonCode": "3-12",
+        "link": "https://drive.google.com/file/d/1KgWtxQV8QI-rm9xEFGrvHFpcG4v2-L37/view?usp=drive_link",
+        "name": "Augmented triad"
+    },      {
+        "solomonCode": "4-1",
+        "link": "https://drive.google.com/file/d/1wlBcPDZJ5ci4KyflLdxDCHMy6vm2d-bR/view?usp=drive_link",
+        "name": "Chromatic tetrachord - X Cell"
+    },      {
+        "solomonCode": "4-2",
+        "link": "www.blank.com",
+        "name": "Major second tetracluster no2"
+    },      {
+        "solomonCode": "4-2B",
+        "link": "www.blank.com",
+        "name": "Major second tetracluster no1"
+    },      {
+        "solomonCode": "4-3",
+        "link": "https://drive.google.com/file/d/1nkPmyWMbuu1TvBF0OKWHcCcw5YXTqgYJ/view?usp=drive_link",
+        "name": "mMaj9(no5) - Alternating tetracord"
+    },      {
+        "solomonCode": "4-4",
+        "link": "ww.blank.com",
+        "name": "Minor third tetracluster no2"
+    },      {
+        "solomonCode": "4-4B",
+        "link": "ww.blank.com",
+        "name": "Minor third tetracluster no1"
+    },      {
+        "solomonCode": "4-5",
+        "link": "https://drive.google.com/file/d/12WC3EZsdWl3tpmDCsRhEFTYvSlzwo__c/view?usp=drive_link",
+        "name": "Major third tetracluster n2 - 1 2b 5 7"
+    },      {
+        "solomonCode": "4-5",
+        "link": "www.no.com",
+        "name": "Major third tetracluster no1"
+    },      {
+        "solomonCode": "4-6",
+        "link": "ww.no.com",
+        "name": "Perfect fourth tetracord"
+    },      {
+        "solomonCode": "4-7",
+        "link": "https://drive.google.com/file/d/1ZuJwArdTyzSDMAtdIqwO91dmyBqAUIfP/view?usp=drive_link",
+        "name": "Arab tetracord - 1 2# 3 7"
+    },      {
+        "solomonCode": "4-8",
+        "link": "https://drive.google.com/file/d/1Gwd7rKv25qYGT-K8hfKr44MjsxxLKSVS/view?usp=drive_link",
+        "name": "Double fourth tetracord - 1 4# 5 7"
+    },      {
+        "solomonCode": "4-9",
+        "link": "https://drive.google.com/file/d/1cEs98yxFmAw66WaC9GULTR5PfkEppEgr/view?usp=drive_link",
+        "name": "Double tritone tetracord"
+    },      {
+        "solomonCode": "4-10",
+        "link": "www.no.com",
+        "name": "Minor tetracord"
+    },      {
+        "solomonCode": "4-11",
+        "link": "https://drive.google.com/file/d/1DPvJgg106f2LrGzX2X6sr2YxvliLzsYS/view?usp=drive_link",
+        "name": "Maj9(no5) - Phrygian tetracord"
+    },      {
+        "solomonCode": "4-11B",
+        "link": "https://drive.google.com/file/d/1Par1hjp12OE705FJexzxViMvmPumrgT5/view?usp=drive_link",
+        "name": "m9(no5) - Major tetracord"
+    },      {
+        "solomonCode": "4-12",
+        "link": "https://drive.google.com/file/d/15Y5NtFR4_zp_uTB8awzk0HkJqS3ExiXz/view?usp=drive_link",
+        "name": "7(9b no5) - mMaj6(no5) - Minor Harmonic tetracord"
+    },      {
+        "solomonCode": "4-12B",
+        "link": "https://drive.google.com/file/d/192029FjmwF8DrkPUCLLt4KHM8X31F0eA/view?usp=drive_link",
+        "name": "Diminished major third tetracord - 1 2b 3b 6"
+    },      {
+        "solomonCode": "4-13",
+        "link": "https://drive.google.com/file/d/1zqTXP2Ofl3ft3GVmi30DWuby0Y1v4NCg/view?usp=drive_link",
+        "name": "Diminish 9b tetrachord - 1 2 3b 6"
+    },      {
+        "solomonCode": "4-13B",
+        "link": "https://drive.google.com/file/d/1fd_G1dP6B9-xIUm2ZP_odlq2Rtz-WeaC/view?usp=drive_link",
+        "name": "Diminish perfect fourth tetrachord - 1 2 3b 6"
+    },      {
+        "solomonCode": "4-14",
+        "link": "https://drive.google.com/file/d/1XptitRDhQCnGIAZAa4_8AI8X1_KuIHWm/view?usp=drive_link",
+        "name": "m9(no7) - Maj6(no5) - Minor second major tetrachord "
+    },      {
+        "solomonCode": "4-14B",
+        "link": "https://drive.google.com/file/d/1IdLLelAdqGUvlIUpozfyCynHvC7_npnm/view?usp=drive_link",
+        "name": "Major perfect fourth tetrachord - 1 2 5 7"
+    },      {
+        "solomonCode": "4-15",
+        "link": "https://drive.google.com/file/d/15viK4g-DyY6S8U-7XRXboxuZ3JDX1zKs/view?usp=drive_link",
+        "name": "mMaj(11 no5) - All interval tetrachord no1"
+    },      {
+        "solomonCode": "4-15B",
+        "link": "https://drive.google.com/file/d/1ae1eIosAlDl5vYd8stS2PPt4s0VtoHv_/view?usp=drive_link",
+        "name": "7(9# no5) - All interval tetrachord no2"
+    },      {
+        "solomonCode": "4-16",
+        "link": "https://drive.google.com/file/d/1DpqCV9KUmLbonWR_ieaXiePDLkaSc0GZ/view?usp=drive_link",
+        "name": "Maj(4# no5) - Minor second Cuartal tetracord"
+    },      {
+        "solomonCode": "4-16B",
+        "link": "https://drive.google.com/file/d/1WHlYxdWF01w0PRs-OCJTvYUEI0Bzfu0g/view?usp=drive_link",
+        "name": "7(4 no5) - Cuartal tritone tetrachord"
+    },      {
+        "solomonCode": "4-17",
+        "link": "https://drive.google.com/file/d/1WpV2g-DfCuGPdkXz6869qdWzMhoGtP4k/view?usp=drive_link",
+        "name": "Bitonal - Major/Minor tetrachord - 1 3b 3 5"
+    },      {
+        "solomonCode": "4-18",
+        "link": "https://drive.google.com/file/d/1EnPOrZZD7DKV8-u1jwu5E3SSZwh9pdqc/view?usp=drive_link",
+        "name": "Dim Maj7 - Major diminish tetrachord"
+    },      {
+        "solomonCode": "4-18B",
+        "link": "https://drive.google.com/file/d/1XfHvEqVF8XYwd3Yw6JqIsiEN9zoo8Vlu/view?usp=drive_link",
+        "name": "Minor diminish tetrachord"
+    },      {
+        "solomonCode": "4-19",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "4-19B",
+        "link": "",
+        "name": ""
+    },      {
         "solomonCode": "0-0",
         "link": "",
         "name": ""
-    }
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    },      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    } 
+
 ];
 
 

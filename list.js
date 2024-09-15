37614ec031dc95c6759ef940ac51afc4118ebb8c
siroshimadenagasaki <161388775+siroshimadenagasaki@users.noreply.github.com>
2024-09-15 18:46:44 -0300

    new

---
+++ list.js
@@ -0,0 +1,36 @@
+const solomonlist = [
+    {
+        "solomonCode": "3-1",
+        "link": "https://drive.google.com/file/d/1VF9ZlCfnjY--pqIb-k0shD4WZrkOXNAH/view?usp=drive_link",
+        "name": "Chromatic tricord"
+    },
+    {
+        "solomonCode": "3-2",
+        "link": "",
+        "name": "Phrygian triad"
+    },
+    {
+        "solomonCode": "3-2B",
+        "link": "",
+        "name": "Minor tricord"
+    },
+    {
+        "solomonCode": "3-3",
+        "link": "https://drive.google.com/file/d/1mt0iooczHY7eGaUSPCR245dbKuvyL4ZP/view?usp=drive_link",
+        "name": "mMaj7(no5) - (Major/minor triad n1)"
+    },
+    {
+        "solomonCode": "3-3B",
+        "link": "www.google.com",
+        "name": " Major/minor triad n2 "
+    },
+      {
+        "solomonCode": "0-0",
+        "link": "",
+        "name": ""
+    }
+];
+
+
+
+
